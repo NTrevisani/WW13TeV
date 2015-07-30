424afed33b343216ce9a7384e936232063933613
NTrevisani <nicolo.trevisani@cern.ch>
2015-07-30 11:12:13 +0200

    new source

---
+++ setup/setup.sh
@@ -0,0 +1,6 @@
+source /cvmfs/cms.cern.ch/cmsset_default.sh
+source /gpfs/csic_projects/cms/sw/ROOT/current/root/bin/thisroot.sh
+export PYTHONPATH=$PYTHONPATH:/opt/glite/lib
+
+export PAFPATH=/gpfs/csic_projects/cms/PROOF/paf 
+export PATH=$PAFPATH/bin:$PATH 
